da97b3b4675bbfaa2849b384c9e0d176d88dabf3
yana.konstantynova <yana.konstantynova@gmail.com>
2022-07-07 13:16:16 -0700

    refactoring

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,15 @@
+const API_KEY = '8c1a5de769d96a58761ba1590334a80d';
+
 class Weather {
     constructor(city, country) {
-        this.apiKey = '8c1a5de769d96a58761ba1590334a80d';
         this.city = city;
         this.country = country;
         this.units = 'metric'
     }
     // fetch weather from API
     async getWeather() {
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&${this.country}&appid=${this.apiKey}&units=${this.units}`);
+        const scheme = location.protocol.startsWith('https') ? 'https' : 'http';
+        const response = await fetch(`${scheme}://api.openweathermap.org/data/2.5/weather?q=${this.city}&${this.country}&appid=${API_KEY}&units=${this.units}`);
         const responseData = await response.json();
         return responseData;
 
